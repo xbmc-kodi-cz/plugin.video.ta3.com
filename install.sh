645070c163e0372ed0ac84fe3b6f84143f1a3c0f
mx3L <mx3ldev@gmail.com>
2016-05-08 21:35:50 +0200

    fix listing

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 # do NOT use this script from XBMC addons directory, it is intented for development only
-DESTDIR=~/.xbmc/addons/plugin.video.ta3.com
+DESTDIR=~/.kodi/addons/plugin.video.ta3.com
 
 rm -rf ${DESTDIR}
 mkdir -p ${DESTDIR}
